5be2d70b3e47df243591faf9e68377c1bae4c7a4
Руслан Корнев <iLLuminated_89@mail.ru>
2024-10-22 18:19:51 +0500

    Task3

---
+++ Tyuiu.KornevRM.Sprint3.Task3.V3.Test/DataServiceTest.cs
@@ -0,0 +1,21 @@
+using Tyuiu.KornevRM.Sprint3.Task3.V3.Lib;
+namespace Tyuiu.KornevRM.Sprint3.Task3.V3.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            string value = "cvbmzff orffgtrr dkfvfffdr";
+            char item = 'f';
+            int res = ds.GetMinCharCount(value,item);
+            int wait = 8;
+            Assert.AreEqual(wait,res);
+
+
+
+        }
+    }
+}
---
+++ Tyuiu.KornevRM.Sprint3.Task3.V3/Program.cs
@@ -0,0 +1,23 @@
+﻿using Tyuiu.KornevRM.Sprint3.Task3.V3.Lib;
+namespace Tyuiu.KornevRM.Sprint3.Task3.V3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           DataService ds = new DataService();
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            string value = "cvbmzff orffgtrr dkfvfffdr";
+            char item = 'f';
+            Console.WriteLine("строка: " + value);
+            Console.WriteLine("символ: " + item);
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("Количество символов = "+ ds.GetMinCharCount(value,item));
+            Console.ReadKey();
+        }
+    }
+}
