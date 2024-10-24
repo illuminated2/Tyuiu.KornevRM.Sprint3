e8e5beb646265d9268aaf78a28b8eb9cfff4a377
Руслан Корнев <iLLuminated_89@mail.ru>
2024-10-24 19:27:11 +0500

    Task6

--- Tyuiu.KornevRM.Sprint3.Task6.V17.Lib/DataService.cs
+++ Tyuiu.KornevRM.Sprint3.Task6.V17.Lib/DataService.cs
@@ -1,7 +1,7 @@
 ﻿using tyuiu.cources.programming.interfaces.Sprint3;
 namespace Tyuiu.KornevRM.Sprint3.Task6.V17.Lib
 {
-    public class DataService : ISprint3Task6V14
+    public class DataService : ISprint3Task6V17
     {
         public int GetSumTheDivisors(int startValue, int stopValue)
         {
