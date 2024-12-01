68ba452846b248891c8d2a1cf48b84e7a9e8713f
illuminated <iLLuminated_89@mail.ru>
2024-12-01 16:27:32 +0500

    Task6

--- Tyuiu.KornevRM.Sprint3.Task6.V17/Program.cs
+++ Tyuiu.KornevRM.Sprint3.Task6.V17/Program.cs
@@ -6,6 +6,19 @@ namespace Tyuiu.KornevRM.Sprint3.Task6.V17
         static void Main(string[] args)
         {
             DataService ds = new DataService();
+
+            Console.Title = "Спринт #3 | Выполнил: Корнев Р. М. | ИСПб-24-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #3                                                               *");
+            Console.WriteLine("* Тема: Алгоритмы циклической структуры                                   *");
+            Console.WriteLine("* Задание #6                                                              *");
+            Console.WriteLine("* Вариант #17                                                             *");
+            Console.WriteLine("* Выполнил: Корнев Р. М. | ИСПб-24-1                                      *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая ищет среди целыъ чисел, принадлежащих       *");
+            Console.WriteLine("* числовому отрезку [10, 20]                                              *");
+            Console.WriteLine("*                                                                         *");
             Console.WriteLine("***************************************************************************");
             Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
             Console.WriteLine("***************************************************************************");
