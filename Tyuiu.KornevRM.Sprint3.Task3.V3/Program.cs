fa0a2848300800e0c1c00134e05f77240fb45f27
illuminated <iLLuminated_89@mail.ru>
2024-12-01 16:20:03 +0500

    Task3

--- Tyuiu.KornevRM.Sprint3.Task3.V3/Program.cs
+++ Tyuiu.KornevRM.Sprint3.Task3.V3/Program.cs
@@ -6,6 +6,18 @@ namespace Tyuiu.KornevRM.Sprint3.Task3.V3
         static void Main(string[] args)
         {
            DataService ds = new DataService();
+            Console.Title = "Спринт #3 | Выполнил: Корнев Р. М. | ИСПб-24-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #3                                                               *");
+            Console.WriteLine("* Тема: Алгоритмы циклической структуры                                   *");
+            Console.WriteLine("* Задание #3                                                              *");
+            Console.WriteLine("* Вариант #3                                                              *");
+            Console.WriteLine("* Выполнил: Корнев Р. М. | ИСПб-24-1                                      *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Используя цикл foreach заменить буквы                                   *");
+            Console.WriteLine("* g  на * в строке: cvbmzff orffgtrr dkfvfffdr                            *");
+            Console.WriteLine("*                                                                         *");
             Console.WriteLine("***************************************************************************");
             Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
             Console.WriteLine("***************************************************************************");
