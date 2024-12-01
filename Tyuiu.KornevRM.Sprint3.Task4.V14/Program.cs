d8c840b46da870e00d99d315e1063739f968bf1c
illuminated <iLLuminated_89@mail.ru>
2024-12-01 16:23:23 +0500

    Task4

--- Tyuiu.KornevRM.Sprint3.Task4.V14/Program.cs
+++ Tyuiu.KornevRM.Sprint3.Task4.V14/Program.cs
@@ -6,6 +6,20 @@ namespace Tyuiu.KornevRM.Sprint3.Task4.V14
         static void Main(string[] args)
         {
             DataService ds = new DataService();
+            Console.Title = "Спринт #3 | Выполнил: Корнев Р. М. | ИСПб-24-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #3                                                               *");
+            Console.WriteLine("* Тема: Алгоритмы циклической структуры                                   *");
+            Console.WriteLine("* Задание #4                                                              *");
+            Console.WriteLine("* Вариант #14                                                             *");
+            Console.WriteLine("* Выполнил: Корнев Р. М. | ИСПб-24-1                                      *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* На отрезке, где x принимает значения от -5 до 5, вычислить значение     *");
+            Console.WriteLine("* функции y=cos(x)/sin(x) при х = 0,прервать цикл. Полученные значения    *");
+            Console.WriteLine("* суммировать.                                                            *");
+            Console.WriteLine("* пропустить значение Полученные значения перемножать.                    *");
+            Console.WriteLine("*                                                                         *");
             Console.WriteLine("***************************************************************************");
             Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
             Console.WriteLine("***************************************************************************");
